8fbc3c5d6f8386dfe19a1118457b63c04212a5ff
jmausolf <jgm346@gmail.com>
2016-11-09 03:34:57 -0600

    Updating default custom stopwords

--- Topic_Models_for_Text/custom_stopword_tokens.py
+++ Topic_Models_for_Text/custom_stopword_tokens.py
@@ -1,5 +1,4 @@
-#Examples
+#Examples:
 #custom_stopwords = [] #No custom stopwords
 #custom_stopwords = ["immediate", "release", "remarks", "president"]
-
-custom_stopwords = ["http", "url", "www", "cite", "immediate", "release", "remarks", "weekly", "address", "president", "obama", "white", "house", "office", "press", "secretary", "applause", "united", "states", "government", "new", "york", "end", "edt"]
+custom_stopwords = ["http", "https", "rt", "url", "www", "cite", "immediate", "release", "remarks", "weekly", "address", "president", "obama", "white", "house", "office", "press", "secretary", "applause", "united", "states", "government", "new", "york", "end", "edt"]
